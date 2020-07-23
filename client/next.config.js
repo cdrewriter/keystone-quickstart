523b90cd77ca050f8dcef1f57cc8913eb14ef78f
Dmitriy Agapov <pr-web2006@yandex.ru>
2020-07-23 05:24:06 +0500

    com2

--- client/next.config.js
+++ client/next.config.js
@@ -5,6 +5,6 @@ const { distDir } = require('../config');
 module.exports = withGraphQLConfig(
   withSass({
     dev: process.env.NODE_ENV === 'development',
-    distDir: `../${distDir}/www`,
+    distDir: `../${distDir}`,
   })
 );
