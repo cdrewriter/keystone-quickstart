193f1ed960c07871c4216c58fe5109c0e0099524
Dmitriy Agapov <pr-web2006@yandex.ru>
2020-07-31 01:58:04 +0500

    serv

--- types2/Category.js
+++
@@ -1,8 +0,0 @@
-import PropTypes from 'prop-types';
-
-export const categoryPropTypes = {
-  id: PropTypes.string,
-  name: PropTypes.string,
-  slug: PropTypes.string,
-  description: PropTypes.string,
-};
--- types2/index.js
+++
@@ -1,6 +0,0 @@
-export * from './File';
-export * from './Category';
-export * from './User';
-export * from './Post';
-export * from './QueryMeta';
-export * from './PageInfo';
